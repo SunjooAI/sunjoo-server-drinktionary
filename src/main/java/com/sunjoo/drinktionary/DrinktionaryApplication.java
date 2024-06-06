f475230532e7c00948ce5bb60714f2263890abc4
SunYerim <yaelim6@pukyong.ac.kr>
2024-06-06 12:39:59 +0900

    feat: project 생성

---
+++ src/main/java/com/sunjoo/drinktionary/DrinktionaryApplication.java
@@ -0,0 +1,13 @@
+package com.sunjoo.drinktionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinktionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinktionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunjoo/drinktionary/DrinktionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunjoo.drinktionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinktionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
