d9eb08797a670624510b4a0576a83b88d1d95833
SunYerim <yaelim6@pukyong.ac.kr>
2024-06-20 01:14:49 +0900

    feat: #8. review, drink Service코드 작성

--- src/main/java/com/sunjoo/drinktionary/service/DrinkService.java
+++ src/main/java/com/sunjoo/drinktionary/service/DrinkService.java
@@ -1,5 +1,6 @@
 package com.sunjoo.drinktionary.service;
 
+import com.sunjoo.drinktionary.entity.DrinkType;
 import com.sunjoo.drinktionary.repository.DrinkRepository;
 import com.sunjoo.drinktionary.dto.DrinkResponse;
 import com.sunjoo.drinktionary.entity.Drink;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,11 +23,47 @@ public class DrinkService {
     }
 
     // 전체 조회
-//    public List<DrinkResponse> getAllDrinks() {
-//        List<Drink> drinks = drinkRepository.findAll();
-//
-//
-//    }
+    public List<DrinkResponse> getAllDrinks() {
+        List<Drink> drinks = drinkRepository.findAll();
 
+        return drinks.stream()
+                .map(drink -> DrinkResponse.response(drink,
+                        reviewService.getAverageScore(drink.getId()),
+                        reviewService.getReviewSize(drink.getId())))
+                .collect(Collectors.toList());
+
+    }
+
+    // 개별 조회
+    public DrinkResponse getDrinkById(final Long id) {
+        final Drink drink = drinkRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 주류입니다."));
+
+        return DrinkResponse.response(drink, reviewService.getAverageScore(id), reviewService.getReviewSize(id));
+    }
+
+    // 타입 조회
+    public List<DrinkResponse> getDrinksByType(final DrinkType type) {
+        final List<Drink> drinks = drinkRepository.findByType(type);
+
+        return drinks.stream()
+                .map(drink -> DrinkResponse.response(drink,
+                        reviewService.getAverageScore(drink.getId()),
+                        reviewService.getReviewSize(drink.getId())))
+                .toList();
+    }
+
+    // 이름 조회
+    public List<DrinkResponse> getDrinksByName(String name) {
+        final List<Drink> drinks = drinkRepository.findAll().stream()
+                .filter(drink -> drink.getName().contains(name))
+                .toList();
+
+        return drinks.stream()
+                .map(drink -> DrinkResponse.response(drink,
+                        reviewService.getAverageScore(drink.getId()),
+                        reviewService.getReviewSize(drink.getId())))
+                .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/com/sunjoo/drinktionary/service/ReviewService.java
+++ src/main/java/com/sunjoo/drinktionary/service/ReviewService.java
@@ -1,19 +1,51 @@
 package com.sunjoo.drinktionary.service;
 
+import com.sunjoo.drinktionary.dto.ReviewResponses;
+import com.sunjoo.drinktionary.entity.Review;
 import com.sunjoo.drinktionary.repository.DrinkRepository;
 import com.sunjoo.drinktionary.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+
 @Service
 public class ReviewService {
 
-//    private final ReviewRepository reviewRepository;
-//    private final DrinkRepository drinkRepository;
-//
-//    public ReviewService(ReviewRepository reviewRepository, DrinkRepository drinkRepository) {
-//        this.reviewRepository = reviewRepository;
-//        this.drinkRepository = drinkRepository;
-//    }
+    private final ReviewRepository reviewRepository;
+    private final DrinkRepository drinkRepository;
+
+    public ReviewService(ReviewRepository reviewRepository, DrinkRepository drinkRepository) {
+        this.reviewRepository = reviewRepository;
+        this.drinkRepository = drinkRepository;
+    }
+
+    // 리뷰 조회
+    public ReviewResponses getReviews(Long drinkId) {
+        final String name = drinkRepository.findById(drinkId).get().getName();
+
+        return ReviewResponses.fromReviews(getReviewsByDrinkName(name));
+    }
+
+    public List<Review> getReviewsByDrinkName(final String name) {
+        return reviewRepository.findByDrinkName(name).stream()
+                .sorted(comparing(Review::getCreateAt).reversed())
+                .collect(Collectors.toList());
+    }
+
+    public int getReviewSize(long drinkId) {
+        return reviewRepository.countByDrinkId(drinkId);
+    }
+
+    public double getAverageScore(long drinkId) {
+        final List<Review> reviews = reviewRepository.findByDrinkId(drinkId);
+        double total = reviews.stream()
+                .mapToDouble(Review::getStarRating)
+                .sum();
+        return Math.round(total / (double) reviews.size() * 100) / 100.0;
+    }
 
 
 }
