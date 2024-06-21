cd75f65d12efdb2a853598b9c1103a9c44f149d1
SunYerim <yaelim6@pukyong.ac.kr>
2024-06-21 18:22:57 +0900

    feat: #9. 주류 랭킹별 조회 api - service코드

--- src/main/java/com/sunjoo/drinktionary/repository/DrinkRepository.java
+++ src/main/java/com/sunjoo/drinktionary/repository/DrinkRepository.java
@@ -2,6 +2,7 @@ package com.sunjoo.drinktionary.repository;
 
 import com.sunjoo.drinktionary.entity.Drink;
 import com.sunjoo.drinktionary.entity.DrinkType;
+import com.sunjoo.drinktionary.entity.Sentiment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +17,7 @@ public interface DrinkRepository extends JpaRepository<Drink, Long> {
     List<Drink> findByName(String name);
     // 주류 종류별 조회
     List<Drink> findByType(DrinkType type);
+    // 감정별 주류 조회
+    List<Drink> findBySentiment(Sentiment sentiment);
+
 }
--- src/main/java/com/sunjoo/drinktionary/service/DrinkService.java
+++ src/main/java/com/sunjoo/drinktionary/service/DrinkService.java
@@ -1,5 +1,6 @@
 package com.sunjoo.drinktionary.service;
 
+import com.sunjoo.drinktionary.dto.DrinkWithRatingResponse;
 import com.sunjoo.drinktionary.entity.DrinkType;
 import com.sunjoo.drinktionary.repository.DrinkRepository;
 import com.sunjoo.drinktionary.dto.DrinkResponse;
@@ -9,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparingInt;
 
 @Service
 @Transactional(readOnly = true)
@@ -64,6 +67,27 @@ public class DrinkService {
                         reviewService.getAverageScore(drink.getId()),
                         reviewService.getReviewSize(drink.getId())))
                 .collect(Collectors.toList());
+    }
+
+    // 평점 조회
+    public List<DrinkWithRatingResponse> getDrinksWithRating() {
+        return drinkRepository.findAll().stream() // 모든 음료를 조회하고 스트림으로 변환
+                .map(Drink::getName) // 각 음료 객체를 음료 이름으로 변환
+                .map(reviewService::getReviewsByDrinkName) // 음료 이름으로 리뷰 목록을 조회
+                .filter(list -> !list.isEmpty()) // 리뷰 목록이 비어 있지 않은 경우만 필터링
+                .map(DrinkWithRatingResponse::createFromRating) // 리뷰 목록을 DrinkWithRatingResponse 객체로 변환
+                .toList(); // 최종 결과를 리스트로 변환하여 반환
+
+    }
+
+    public List<DrinkWithRatingResponse> findTop5ByReviews() {
+        final List<DrinkWithRatingResponse> drinks = getDrinksWithRating(); // 모든 음료와 리뷰 정보를 가져옴
+
+        return drinks.stream()
+                .filter(drink -> drink.getReviewCount() != 0) // 리뷰 수가 0이 아닌 경우만 필터링
+                .sorted(comparingInt(DrinkWithRatingResponse::getReviewCount).reversed()) // 리뷰 수를 기준으로 내림차순 정렬
+                .limit(5) // 상위 5개만 선택
+                .toList(); // 최종 결과를 리스트로 변환하여 반환
     }
 
 }
