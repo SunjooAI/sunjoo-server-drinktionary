4090e824df3a42b63d08e8503c33a64ea67717f1
SunYerim <yaelim6@pukyong.ac.kr>
2024-06-21 18:15:44 +0900

    feat: #9. ReviewRequest, ratingresponse dto 작성,

---
+++ src/main/java/com/sunjoo/drinktionary/dto/WriteReviewRequest.java
@@ -0,0 +1,18 @@
+package com.sunjoo.drinktionary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WriteReviewRequest {
+
+    private long userId;
+    private String comment;
+    private int score;
+    private String date;
+}
