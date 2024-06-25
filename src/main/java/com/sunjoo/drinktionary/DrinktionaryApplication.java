3b63bbe105775d351acf00c139c5f9d0053fadfc
SunYerim <yaelim6@pukyong.ac.kr>
2024-06-26 00:41:52 +0900

    feat: #9. 리뷰등록 controller 작성 및 application feignClient 빈 선언

--- src/main/java/com/sunjoo/drinktionary/DrinktionaryApplication.java
+++ src/main/java/com/sunjoo/drinktionary/DrinktionaryApplication.java
@@ -2,8 +2,10 @@ package com.sunjoo.drinktionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.sunjoo.drinktionary.client")
 public class DrinktionaryApplication {
 
 	public static void main(String[] args) {
