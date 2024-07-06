1e37101d89c9271495c5014bfdbccf23addd0e8e
SunYerim <yaelim6@pukyong.ac.kr>
2024-07-06 23:42:17 +0900

    fix: feignClient url수정

--- src/main/java/com/sunjoo/drinktionary/client/UserClient.java
+++ src/main/java/com/sunjoo/drinktionary/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
-@FeignClient(name = "auth-service", url = "http://sunjoo-server-auth-spring-1:8090")
+@FeignClient(name = "auth-service", url = "http://13.124.194.48:8090")
 public interface UserClient {
 
     @GetMapping("/auth/userinfo")
